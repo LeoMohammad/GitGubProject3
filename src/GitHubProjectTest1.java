e8024b4006f785467e6f1491b65581d2ce8fef59
Mohammad Habibi <shabirhabibi426@gmail.com>
2023-04-22 23:07:49 +0100

    king

--- src/GitHubProjectTest1.java
+++ src/GitHubProjectTest1.java
@@ -1,4 +1,4 @@
-public class GithubProjectClass1 {
+public class GitHubProjectTest1 {
     public static void main(String[] args) {
         System.out.println("Hello github world ");
     }
---
+++ src/GitHubProjectTest2.java
@@ -0,0 +1,8 @@
+public class GitHubProjectTest2 {
+    public static void main(String[] args) {
+
+        System.out.println("test project class 2");
+
+
+    }
+}
